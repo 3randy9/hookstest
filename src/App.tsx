c7f0b915e1b401b9298f9b24134a80c0408ef17e
randy39 <3randy9@gmail.com>
2019-09-08 16:31:09 +0900

    setState

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App: React.FC = () => {
-  return (
-    <div>
-     React app
-    </div>
-  );
-}
+	const [count, setCount] = useState(0);
+
+	const increment = () => setCount(count + 1);
+	const decrement = () => setCount(count - 1);
+	const increment2 = () => setCount(previousCount => previousCount + 1);
+	const decrement2 = () => setCount(previousCount => previousCount - 1);
+	const clear = () => setCount(0);
+	const double = () => setCount(count * 2);
+	const third = () => setCount(count % 3 === 0 ? count / 3 : count);
+
+	return (
+		<>
+			<div>count: {count}</div>
+			<div>
+				<button onClick={increment}>+1</button>
+				<button onClick={decrement}>-1</button>
+			</div>
+			<div>
+				<button onClick={increment2}>+1</button>
+				<button onClick={decrement2}>-1</button>
+			</div>
+			<div>
+				<button onClick={clear}>Reset</button>
+				<button onClick={double}>×2</button>
+				<button onClick={third}>3の倍数の値だけ3で割る</button>
+			</div>
+		</>
+	);
+};
 
 export default App;
