76b09f3bdd35f10d805f9ad24b9b5dd80f0ad948
randy39 <3randy9@gmail.com>
2019-09-08 23:49:56 +0900

    [add] index reducer

---
+++ src/reducers/index.ts
@@ -0,0 +1,36 @@
+// action = {
+//		type: 'CREATE_EVENT',
+// 		title: '2020年東京オリンピックのお知らせ',
+// 		body: '2020年に東京オリンピックを開催します。'
+// }
+
+interface EventState {
+	id: number;
+	title: string;
+	body: string;
+}
+
+interface Action extends EventState {
+	type: string;
+}
+
+type State = EventState[];
+
+const events = (state: State = [], action: Action) => {
+	switch (action.type) {
+		case 'CREATE_EVENT':
+			const event = { title: action.title, body: action.body };
+			const length = state.length;
+			const id = length === 0 ? 1 : state[length - 1].id + 1;
+			return [...state, { id, ...event }];
+
+		case 'DELETE_EVENT':
+			return state;
+		case 'DELETE_ALL_EVENT':
+			return state;
+		default:
+			return state;
+	}
+};
+
+export default events;
