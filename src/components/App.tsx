4885ca82d6ec2daed533ef20bfc5ff73cad33841
randy39 <3randy9@gmail.com>
2019-09-29 16:14:51 +0900

    [Complete] state to LocalStorage

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import reducer from '../reducers';
 import AppContext from '../contexts/AppContext';
@@ -6,12 +6,20 @@ import EventForm from './EventForm';
 import EventList from './EventList';
 import OperationList from './OperationList';
 
+const APP_KEY = 'appWithRedux'
+
 const App = () => {
-	const initialState = {
+	const appState = localStorage.getItem(APP_KEY);
+	const initialState = appState ? JSON.parse(appState) : {
 		events: [],
 		operationLogs: []
 	};
 	const [state, dispatch] = useReducer(reducer, initialState);
+
+	useEffect(() => {
+		const stateToJson = JSON.stringify(state);
+		localStorage.setItem(APP_KEY, stateToJson);
+	}, [state]);
 
 	return (
 		<AppContext.Provider value={{ state, dispatch }}>
